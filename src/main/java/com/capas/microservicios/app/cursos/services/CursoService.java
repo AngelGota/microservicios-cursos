3b193bbc77e63b7eb6e05858fea4dd48a6fd66af
Angelo <gangelos630@gmail.com>
2024-03-16 01:48:10 -0500

    inicializacion de microservicio de cursos

---
+++ src/main/java/com/capas/microservicios/app/cursos/services/CursoService.java
@@ -0,0 +1,8 @@
+package com.capas.microservicios.app.cursos.services;
+
+import com.capas.microservicios.app.commons.services.CommonService;
+import com.capas.microservicios.app.cursos.models.entity.Curso;
+
+public interface CursoService extends CommonService<Curso> {
+
+}
---
+++ src/main/java/com/capas/microservicios/app/cursos/services/CursoServiceImplement.java
@@ -0,0 +1,9 @@
+package com.capas.microservicios.app.cursos.services;
+
+import com.capas.microservicios.app.commons.services.CommonServiceImplement;
+import com.capas.microservicios.app.cursos.models.entity.Curso;
+import com.capas.microservicios.app.cursos.models.repository.CursoRepository;
+
+public class CursoServiceImplement extends CommonServiceImplement<Curso, CursoRepository> implements CursoService {
+
+}
